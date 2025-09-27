2d54a2eab5fe05d84175ba7fe977ae6c3fb582c8
egoranuchin <egoranuchin@gmail.com>
2025-09-27 13:41:03 +0200

    link descriptions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home(): ReactNode {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Documentation for creating an AI Agent with no-code">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
